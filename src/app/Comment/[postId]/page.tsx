1de3b4d65f223b51beec938176f158806615dbe2
25HP4550 25HP4550 <25HP4550@G301-CC0188.local>
2025-10-18 09:13:29 +0800

    postId

--- src/app/Comment/[postId]/page.tsx
+++ src/app/Comment/[postId]/page.tsx
@@ -1,9 +1,12 @@
 import { UseUser } from "@/providers/AuthProvider";
+import { useParams } from "next/navigation";
 import { useState } from "react";
 
 const Comment = () => {
   const { token } = UseUser();
   const [getCom, setGetCom] = useState([]);
+  const { params } = useParams();
+  const postId = params.postId;
   const getComment = async () => {
     const coooms = await fetch(`http://localhost:5555/Comment/Get/${postId}`, {
       method: "GET",
@@ -17,15 +20,13 @@ const Comment = () => {
   };
 
   const writeComment = async () => {
-    const coooms = await fetch(`http://localhost:5555/Comment/Create`, {
+    const coooms = await fetch(`http://localhost:5555/Comment/Create${postId}`, {
       method: "POST",
       headers: {
         "Content-type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({
-        comment:
-      })
+      body: JSON.stringify({}),
     });
     const response = await coooms.json();
     setGetCom(response);
